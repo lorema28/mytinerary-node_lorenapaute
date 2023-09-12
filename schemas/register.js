faa38cf61c935f20c5bc4611dc719d71adcb5443
lorema28 <lorenapaute@gmail.com>
2023-09-11 21:32:26 -0300

    schemas

--- schemas/register.js
+++ schemas/register.js
@@ -1,26 +1,39 @@
 import joi from "joi";
 
 let registerSchema = joi.object({
-    name: joi.string().required().min(3).max(20).messages({
-        'string.min': "name must have at least 3 characters please!",
-        "string.max": "name must be less than 21 characters please!",
-        "any.required": "name is required", //para cuando NO se envía el dato
-        "string.empty": "name is required"  //para cuando se envía VACÍO
+    name: joi.string().required().min(4).max(20).messages({
+        'string.min': "name must have at least 4 characters!",
+        'string.max': "name must be less than 20 characters!",
+        'any.required': "name is required", //para cuando NO se envia el dato
+        'string.empty': "name cannot be empty!" //para cuando se envia VACIO
     }),
-    mail: joi.string().required(),
-    password: joi.string().required(),
-    photo: joi.string(),
-  
-    country: joi.string().required(),
-    lastName: joi.string().min(3).max(20).empty("").messages({
-        'string.min': "last name must have at least 3 characters please!",
-        "string.max": "last name must be less than 21 characters please!",
+    lastName: joi.string().min(4).max(20).empty('').messages({
+        'string.min': "lastname must have at least 4 characters!",
+        'string.max': "lastname must be less than 20 characters!"
+    }),
+    country: joi.string().required().messages({
+        'any.required': "country is required",
+        'string.empty': "country cannot be empty!"
+    }),
+    photo: joi.string().uri().messages({
+        'string.uri': 'invalid url'
+    }),
+    mail: joi.string().required().min(8).max(20).email({ minDomainSegments: 2}).messages({
+        'string.min': "mail must have at least 8 characters!",
+        'string.max': "mail must be less than 20 characters!",
+        'any.required': 'mail is required',
+        'string.empty': 'mail cannot be empty!',
+        'string.email': 'invalid email'
+    }),
+    password: joi.string().required().min(8).max(20).messages({
+        'string.min': "password must have at least 8 characters!",
+        'string.max': "password must be less than 20 characters!",
+        'any.required': "password is required",
+        'string.empty': "password cannot be empty!"
     })
 })
 
 export default registerSchema
-
-
 //.min(3).max(200).empty('').messages({
    // 'string.min': "photo must have at least 3 characters please",
  //   'string.max':"photo must have less less than 20 characters or be equal to 20 characters please!"  }),
--- schemas/signin.js
+++ schemas/signin.js
@@ -1,18 +1,19 @@
 import joi from "joi";
 
 let signinSchema = joi.object({
-    mail: joi.string().required().min(8).max(21).messages({
-        'string.min': "mail must have at least 8 characters please!",
-        "string.max": "mail must be less than 21 characters please!",
-        "any.required": "mail is required", //para cuando NO se envía el dato
-        "string.empty": "mail is required"  //para cuando se envía VACÍO
+    mail: joi.string().required().min(8).max(20).email({ minDomainSegments: 2}).messages({
+        'string.min': "mail must have at least 8 characters!",
+        'string.max': "mail must be less than 20 characters!",
+        'any.required': "mail is required", //para cuando NO se envia el dato
+        'string.empty': "mail cannot be empty!", //para cuando se envia VACIO
+        'string.email': 'invalid email'
     }),
     password: joi.string().required().min(8).max(20).messages({
-        'string.min': "password must have at least 8 characters please!",
-        "string.max": "password must be less than 21 characters please!",
-        "any.required": "password is required", //para cuando NO se envía el dato
-        "string.empty": "password is required"  //para cuando se envía VACÍO
-    }),
+        'string.min': "password must have at least 8 characters!",
+        'string.max': "password must be less than 20 characters!",
+        'any.required': "password is required",
+        'string.empty': "password cannot be empty!"
+    })
 })
 
 export default signinSchema
